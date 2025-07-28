64732a3de437c65dd4d5b7e18e6137eb61875a5e
kyw04 <85551017+kyw04@users.noreply.github.com>
2025-07-28 18:21:41 +0900

    update: jump and dash limit

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -11,7 +11,9 @@ public class Mover : MonoBehaviour
     private InputAction moveAction;
 
     private Vector2 currentMove;
-
+    private int jumpCount;
+    private int dashCount;
+    
     public float speed;
     public float jumpPower;
     public float dashPower;
@@ -24,12 +26,14 @@ public class Mover : MonoBehaviour
         moveAction = playerInput.currentActionMap.FindAction("Move");
     }
 
-    private void Update()
-    {
-    }
-
     private void FixedUpdate()
     {
+        if (OnGround())
+        {
+            jumpCount = 0;
+            dashCount = 0;
+        }
+        
         Move();
     }
 
@@ -38,20 +42,52 @@ public class Mover : MonoBehaviour
         Vector2 value = moveAction.ReadValue<Vector2>();
         sprite.flipX = value.x == 0 ? sprite.flipX : value.x > 0 ? true : false;
         Vector2.SmoothDamp(currentMove, new Vector2(value.x, 0), ref currentMove, 2.0f);
-        Debug.Log(currentMove);
         transform.Translate(transform.right * currentMove * speed * Time.deltaTime);
     }
 
+    private bool OnGround()
+    {
+        return Physics2D.BoxCast(transform.position, new Vector2(0.5f, 0.25f), 0, Vector2.down, 0.5f, LayerMask.GetMask("Floor"));
+    }
+
+    private void FreezePositionY()
+    {
+        rigi.linearVelocity = Vector2.zero;
+        rigi.constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    private void MeltPosition()
+    {
+        rigi.constraints = ~RigidbodyConstraints2D.FreezePosition | RigidbodyConstraints2D.FreezeRotation;
+    }
+    
     private void OnJump()
     {
+        if (jumpCount >= 2)
+            return;
+        jumpCount += OnGround() ? 1 : 2;
+        
+        CancelInvoke(nameof(MeltPosition));
+        MeltPosition();
         rigi.linearVelocity = Vector2.zero;
         rigi.AddForce(Vector2.up * jumpPower);
     }
     
     private void OnDash()
     {
+        if (dashCount >= 2)
+            return;
+        dashCount++;
+        
         Vector2 direction = sprite.flipX ? Vector2.right : -Vector2.right;
-        rigi.linearVelocity = Vector2.zero;
+        FreezePositionY();
+        Invoke(nameof(MeltPosition), 0.15f);
         rigi.AddForce(direction * dashPower, ForceMode2D.Force);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = OnGround() ? Color.green : Color.red;
+        Gizmos.DrawWireCube(transform.position + Vector3.down * 0.5f, new Vector2(0.5f, 0.25f));
     }
 }
